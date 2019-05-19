2f7bfecd06677a9373063047682c79657c3efa48
Phako Perez <13.phakman@gmail.com>
2019-05-18 23:33:29 -0500

    Add setter and getter to the Board class.

--- eight_queens.py
+++ eight_queens.py
@@ -14,8 +14,26 @@ class MyBoard:
 
     def generate_board(self):
         self.board = np.stack([0 for x in range(self.b_size * self.b_size)])
-        self.board = self.board.reshapt(self.b_size, self.b_size)
+        self.board = self.board.reshape(self.b_size, self.b_size)
         return self.board
+
+    def set_queen(self, x, y):
+        """
+        We use this function to set a queen into our board
+        :param x: X position
+        :param y: Y position
+        :return: None
+        """
+        self.board[x][y] = 1
+
+    def get_value(self, x, y):
+        """
+        This will check weather a queen is or not at the provided coordinate
+        :param x: X position
+        :param y: Y positio
+        :return: None
+        """
+        return self.board[x][y]
 
     def print_board(self):
         """
@@ -32,13 +50,16 @@ def def_queens_pos(ordered, values, directions):
     :param directions: the list of the coordinates for each directions
     :return: set generated with the new coordinates
     """
-    result = []
+    result = set()
 
     a = random.randrange(len(ordered))
     for tmp_lsit in ordered:
         new_co = validate(tmp_lsit, values, directions)
         if new_co:
-            result.append(tmp_lsit)
+            result.add(tmp_lsit)
+            break
+
+    return result
 
 
 def validate(nxy, vector, board):
@@ -112,6 +133,8 @@ def gen_arrays(s):
         if len(arr_ry) > 1 and arr_ry not in my_arrays:
             my_arrays.append(arr_ry)
 
+    return my_arrays
+
 
 if __name__ == "__main__":
     n = 5
@@ -120,9 +143,23 @@ if __name__ == "__main__":
         print("We need a value grater than 3, you passed {}".format(n))
         n = int(input("Insert new value:"))
 
+    # Get a list of lists containing our coordinates
     valid = gen_arrays(n)
+
+    results = []
+    r_order = valid.copy()
+    random.shuffle(r_order)
+    print("No valid vectors:", len(valid))
+
+    while len(results) < n and len(r_order) > 0:
+        next_list = r_order[0]
+        del r_order[0]
+        results += def_queens_pos(next_list, results, valid)
+
+    print("My results:", results)
 
     myBoard = MyBoard(n)
     myBoard.generate_board()
+
     myBoard.print_board()
 
